957f0c220819c320a920a4bc681d42f413395497
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-02-03 16:14:26 -0500

    Created MusicTable component and instantiated MsuicTable on the App component.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import MusicTable from './Components/MusicTable/MusicTable';
 
 function App() {
 
 const [songs, setSongs] = useState([{title: 'Talking to the Moon' , artist: 'Bruno Mars', album: 'Doo-Wops & Hooligans', release_date: '2010-05-11', genre: 'Pop'}])
 
   return (
-    <div >
-      <table>
-        <thead>
-        <tr>
-          <th>Title</th>
-          <th>Artist</th>
-          <th>Album</th>
-          <th>Release Date</th>
-          <th>Genre</th>
-        </tr>
-        </thead>
-        <tbody>
-          {songs.map((song, index) => {
-            return (
-              <tr>
-                <td>{song.title}</td>
-                <td>{song.artist}</td>
-                <td>{song.album}</td>
-                <td>{song.release_date}</td>
-                <td>{song.genre}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div>
+      <MusicTable songs={songs}/>
     </div>
   );
 }
