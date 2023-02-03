fbdd55e81d3bf3cff4b65dabc7062060c7e692b6
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-02-03 16:02:16 -0500

    Deleted unnecessary files and created folders and files for the SongForm component, MusicTable component and the SearchBar component + created temp table inside of App component + installed axios.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+const [songs, setSongs] = useState([{title: 'Talking to the Moon' , artist: 'Bruno Mars', album: 'Doo-Wops & Hooligans', release_date: '2010-05-11', genre: 'Pop'}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <table>
+        <thead>
+        <tr>
+          <th>Title</th>
+          <th>Artist</th>
+          <th>Album</th>
+          <th>Release Date</th>
+          <th>Genre</th>
+        </tr>
+        </thead>
+        <tbody>
+          {songs.map((song, index) => {
+            return (
+              <tr>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.release_date}</td>
+                <td>{song.genre}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
