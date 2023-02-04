2f06bc33227b59d037298731fad2206953b4ed9a
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2023-02-03 21:28:51 -0500

    Adjusted the code in the App component and commented oout the code in the MusicTable componenet as my data was not yet showing up from the backend requests and database.

--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,32 +1,42 @@
+// import axios from 'axios';
+// import MusicTable from './Components/MusicTable/MusicTable';
 
-const MusicTable = (props) => {
+// useEffect(() => {
+//     getAllSongs();
+//   }, [])
 
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Artist</th>
-                    <th>Album</th>
-                    <th>Release Date</th>
-                    <th>Genre</th>
-                    </tr>
-            </thead>
-            <tbody>
-                {props.songs.map((song) => {
-                    return (
-                        <tr>
-                            <td>{song.title}</td>
-                            <td>{song.artist}</td>
-                            <td>{song.album}</td>
-                            <td>{song.release_date}</td>
-                            <td>{song.genre}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table> 
-    );   
-}
+//   async function getAllSongs(){
+//     let response = await axios.get('http://127.0.0.1:8000/api/music/');
+//     setSongs(response.data);
+//   }
+// const MusicTable = (props) => {
+
+//     return (
+//         <table>
+//             <thead>
+//                 <tr>
+//                     <th>Title</th>
+//                     <th>Artist</th>
+//                     <th>Album</th>
+//                     <th>Release Date</th>
+//                     <th>Genre</th>
+//                     </tr>
+//             </thead>
+//             <tbody>
+//                 {props.songs.map((song) => {
+//                     return (
+//                         <tr>
+//                             <td>{song.title}</td>
+//                             <td>{song.artist}</td>
+//                             <td>{song.album}</td>
+//                             <td>{song.release_date}</td>
+//                             <td>{song.genre}</td>
+//                         </tr>
+//                     );
+//                 })}
+//             </tbody>
+//         </table> 
+//     );   
+// }
  
-export default MusicTable;
+// export default MusicTable;
